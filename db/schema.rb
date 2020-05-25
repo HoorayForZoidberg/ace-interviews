f1929e782b11a9c0c88eb2a6697b91e3f05c3c74
Matthew Gilligan <mcgilligan97@gmail.com>
2020-05-25 13:06:50 +0100

    creating question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_114127) do
+ActiveRecord::Schema.define(version: 2020_05_25_120030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2020_05_25_114127) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["interviewee_id"], name: "index_meetings_on_interviewee_id"
     t.index ["interviewer_id"], name: "index_meetings_on_interviewer_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "industry"
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
