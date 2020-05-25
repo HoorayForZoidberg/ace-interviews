5c8e4d46c588d80fe9394d66df29d42d5c3c607d
Helena Gagern <helena.gagern@gmail.com>
2020-05-25 15:54:59 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :users, only: [:index, :show] do
+
+    resources :meetings, only: [:new, :create]
+  end
+  resources :meetings, only: [:index, :show, :edit, :update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
+
 end
