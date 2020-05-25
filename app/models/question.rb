020b087e483068cdcfe5ae25c3b9158b003149a4
Matthew Gilligan <mcgilligan97@gmail.com>
2020-05-25 14:35:15 +0100

    validating date is in the future and adding industries/types array

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,9 @@
 class Question < ApplicationRecord
   has_many :meetings
 
-  validates :industry, presence: true
-  validates :type, presence: true
+  INDUSTRIES = ["Consumer Goods", "Airlines", "Energy (Oil & Gas / Mining)", "Financial Services (Insurance)", "Manufacturing", "Media", "Pharmaceuticals", "Private Equity", "Technology", "Telecom / Mobile", "Sports", "Other"]
+  TYPES = ["Market Entry", "M&A", "New Product Development", "Pricing", "New Business", "Competitive response",	"Increase Sales", "Reduce Cost", "Increase Profit",	"Turn-around"]
+									
+  validates :industry, inclusion: { in: INDUSTRIES }
+  validates :type, inclusion: { in: TYPES }
 end
