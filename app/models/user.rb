93c3540e74209b6c261ce5ebda98b031fc619b6f
Matthew Gilligan <mcgilligan97@gmail.com>
2020-05-25 13:24:07 +0100

    adding question-meeting foregin key and has manys to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :users_as_interviewee, 
+         foreign_key: :interviewee_id, 
+         class_name: :Meeting
+  has_many :users_as_interviewer, 
+         foreign_key: :interviewer_id, 
+         class_name: :Meeting
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_120030) do
+ActiveRecord::Schema.define(version: 2020_05_25_120934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 2020_05_25_120030) do
     t.boolean "finished", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "question_id", null: false
     t.index ["interviewee_id"], name: "index_meetings_on_interviewee_id"
     t.index ["interviewer_id"], name: "index_meetings_on_interviewer_id"
+    t.index ["question_id"], name: "index_meetings_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -45,4 +47,5 @@ ActiveRecord::Schema.define(version: 2020_05_25_120030) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "meetings", "questions"
 end
