c28dea31447ee2434d572b90e662ed53556dd61c
Morgan Lizop <morgan.lizop@iese.net>
2020-05-25 18:26:46 +0100

    fixes to meeting controller

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,51 +1,53 @@
 class MeetingsController < ApplicationController
-
-def meetings
-  @meetings = Meeting.all
-end
-
-def new
-  @meeting = Meeting.new
-end
-
-def create
-  @meeting = Meeting.new(meeting_params)
-  @meeting.id_interviewee = current_user
-  @meeting.finished = false
-  if @meeting.save
-    redirect_to meeting_path(@meeting)
-  else
-    render :new
+  before_action :find_meeting, only: [:show, :edit, :update, :destroy]
+  def index
+    @meetings = Meeting.where(interviewee_id == current_user.id || interviewer_id == current_user.id)
   end
-end
 
-def show
-  @meeting = Meeting.find(params[:id])
-  @review = Review.new
-end
-
-def edit
-  @meeting = Meeting.new
-end
-
-def update
-  if @meeting.update(meeting_params)
-    redirect_to meeting_path(@meeting)
-  else
-    render :edit
+  def new
+    @meeting = Meeting.new
   end
-end
 
-def destroy
-  @meeting = Meeting.find(params[:id])
-  @meeting.destroy
-  redirect_to index_path
-end
+  def create
+    @meeting = Meeting.new(meeting_params)
+    @meeting.interviewee_id = current_user
+    @meeting.finished = false
+    if @meeting.save
+      redirect_to meeting_path(@meeting)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @review = Review.new
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @meeting.update(meeting_params)
+      redirect_to meeting_path(@meeting)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @meeting.destroy
+    redirect_to index_path
+  end
 
 private
 
-def meeting_params
-  params.require(:meeting).permit(:date, :id_interviewer, :id_question)
-end
+  def meeting_params
+    params.require(:meeting).permit(:date, :interviewer_id, :question_id)
+  end
+
+  def find_meeting
+    @meeting = Meeting.find(params[:id])
+  end
 
 end
