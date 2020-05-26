e6e246d9e489e8ffec298b49964f5c497c9283ee
Morgan Lizop <morgan.lizop@iese.net>
2020-05-26 14:20:12 +0100

    select2 added to university for sign-up. Still needs to be added to edit user page.

---
+++ app/javascript/components/init_select2.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import 'select2';
+
+const initSelect2 = () => {
+  console.log("Hi");
+  $('.select2').select2(); // (~ document.querySelectorAll)
+
+};
+
+export { initSelect2 };
