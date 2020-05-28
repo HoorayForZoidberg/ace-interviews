91adc76077379299117224dc898d9394f383c068
Helena Gagern <helena.gagern@gmail.com>
2020-05-28 14:20:25 +0100

    changes to flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,6 +2,6 @@ import flatpickr from "flatpickr";
 
 document.addEventListener('turbolinks:load', () => {
   if (document.querySelector(".datepicker")) {
-    flatpickr(".datepicker", {enableTime: true, dateFormat: "Y-m-d H:i", minDate: "today"});
+    flatpickr(".datepicker", {enableTime: true, dateFormat: "Y-m-d H:i", minDate: "today", inline: true});
   }
 });
