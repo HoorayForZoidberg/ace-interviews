065a6d99368573a17ebd0cb3ba4496ce82009c79
Matthew Gilligan <mcgilligan97@gmail.com>
2020-05-29 16:25:30 +0100

    temporarily commenting the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -97,73 +97,73 @@ end
 #   puts "added #{@university.name} from #{@university.address}"
 # end
 
-50.times{
-  @question = Question.new()
-  @question.industry = industry.sample
-  @question.discipline = discipline.sample
-  @question.save!
-  puts "added question #{@question.id} about #{@question.discipline} in #{@question.industry}"
-}
+# 50.times{
+#   @question = Question.new()
+#   @question.industry = industry.sample
+#   @question.discipline = discipline.sample
+#   @question.save!
+#   puts "added question #{@question.id} about #{@question.discipline} in #{@question.industry}"
+# }
 
-100.times {
-  @user = User.new()
-  @user.first_name = first_name.sample
-  @user.last_name = last_name.sample
-  @user.password = "TestTest"
-  @user.email = "#{@user.first_name}.#{@user.last_name}#{rand(1..1000)}@gmail.com"
-  @user.address = "#{rand(12..200)} rue de la #{first_name.sample}, #{last_name.sample}ville"
-  @user.university = University.all.sample
-  @user.save!
-  puts "Created #{@user.first_name} #{@user.last_name} who lives at #{@user.address} and who's email is #{@user.email}"
-}
-100.times{
-  @meeting = Meeting.new()
-  @meeting.interviewee = User.all.sample
-  @meeting.interviewer = (User.all - [@meeting.interviewee]).sample
-  @meeting.finished = true
-  @meeting.question = Question.all.sample
-  @meeting.date = DateTime.current + rand(24..240).hours
-  @meeting.save!
-  puts "Created a meeting between #{@meeting.interviewee_id} user and #{@meeting.interviewer_id} on the #{@meeting.date} with question #{@meeting.question_id}"
+# 100.times {
+#   @user = User.new()
+#   @user.first_name = first_name.sample
+#   @user.last_name = last_name.sample
+#   @user.password = "TestTest"
+#   @user.email = "#{@user.first_name}.#{@user.last_name}#{rand(1..1000)}@gmail.com"
+#   @user.address = "#{rand(12..200)} rue de la #{first_name.sample}, #{last_name.sample}ville"
+#   @user.university = University.all.sample
+#   @user.save!
+#   puts "Created #{@user.first_name} #{@user.last_name} who lives at #{@user.address} and who's email is #{@user.email}"
+# }
+# 100.times{
+#   @meeting = Meeting.new()
+#   @meeting.interviewee = User.all.sample
+#   @meeting.interviewer = (User.all - [@meeting.interviewee]).sample
+#   @meeting.finished = true
+#   @meeting.question = Question.all.sample
+#   @meeting.date = DateTime.current + rand(24..240).hours
+#   @meeting.save!
+#   puts "Created a meeting between #{@meeting.interviewee_id} user and #{@meeting.interviewer_id} on the #{@meeting.date} with question #{@meeting.question_id}"
 
-      @review = Review.new()
-      current_user = @meeting.interviewee
-      @review.user_id = @meeting.interviewee_id
-      @review.meeting_id = @meeting.id
+#       @review = Review.new()
+#       current_user = @meeting.interviewee
+#       @review.user_id = @meeting.interviewee_id
+#       @review.meeting_id = @meeting.id
 
-      @review.would_repeat = true
-      @review.polite_engaged = rand(1..5)
+#       @review.would_repeat = true
+#       @review.polite_engaged = rand(1..5)
 
-      @review.structure = rand(1..5)
-      @review.math = rand(1..5)
-      @review.reasoning_originality = rand(1..5)
-      @review.communication = rand(1..5)
-      @review.confidence = rand(1..5)
-      @review.readiness = rand(1..5)
+#       @review.structure = rand(1..5)
+#       @review.math = rand(1..5)
+#       @review.reasoning_originality = rand(1..5)
+#       @review.communication = rand(1..5)
+#       @review.confidence = rand(1..5)
+#       @review.readiness = rand(1..5)
 
-      @review.content = "#{first_word.sample} #{second_word.sample} #{third_word.sample} #{fourth_word.sample}"
+#       @review.content = "#{first_word.sample} #{second_word.sample} #{third_word.sample} #{fourth_word.sample}"
 
-      @review.save!
-      puts "Created a review for interviewer #{@review.user_id}"
+#       @review.save!
+#       puts "Created a review for interviewer #{@review.user_id}"
 
-      @review = Review.new()
-      current_user = @meeting.interviewer
-      @review.user_id = @meeting.interviewer_id
-      @review.meeting_id = @meeting.id
+#       @review = Review.new()
+#       current_user = @meeting.interviewer
+#       @review.user_id = @meeting.interviewer_id
+#       @review.meeting_id = @meeting.id
 
-      @review.would_repeat = true
-      @review.polite_engaged = rand(1..5)
+#       @review.would_repeat = true
+#       @review.polite_engaged = rand(1..5)
 
-      @review.case_knowledge = rand(1..5)
-      @review.case_presentation = rand(1..5)
-      @review.accurate_feedback = rand(1..5)
-      @review.constructive_criticism = rand(1..5)
-      @review.level_adapted = rand(1..5)
-      @review.full_attention = rand(1..5)
+#       @review.case_knowledge = rand(1..5)
+#       @review.case_presentation = rand(1..5)
+#       @review.accurate_feedback = rand(1..5)
+#       @review.constructive_criticism = rand(1..5)
+#       @review.level_adapted = rand(1..5)
+#       @review.full_attention = rand(1..5)
 
-      @review.content = "#{first_word.sample} #{second_word.sample} #{third_word.sample} #{fourth_word.sample}"
+#       @review.content = "#{first_word.sample} #{second_word.sample} #{third_word.sample} #{fourth_word.sample}"
 
-      @review.save!
-      puts "Created a review for interviewee #{@review.user_id}"
-}
+#       @review.save!
+#       puts "Created a review for interviewee #{@review.user_id}"
+# }
 
